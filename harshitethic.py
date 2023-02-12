005214d7f9135fbdcb56b410f03b9dfb8acbfcdd
MD Yaser Arafat Israk <61416887+YaIsrak@users.noreply.github.com>
2023-02-12 12:09:02 +0600

    ewewf
    dfsd

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "5766445831:AAFveJFqbsupRQDqAWP7siGMYobJwTMz2n0"
 else:
     tgkey = tgenv
 print(tgenv)
